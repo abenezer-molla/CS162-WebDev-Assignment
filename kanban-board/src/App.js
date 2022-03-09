0a02ea5aeafebced52768a02646b06c2430f5bf9
Abenezer Molla <57775399+abenezer-molla@users.noreply.github.com>
2022-03-09 02:00:11 +0000

    Working Drag and Drop

--- kanban-board/src/App.js
+++ kanban-board/src/App.js
@@ -1,10 +1,107 @@
+import React, {useState} from 'react';
 
-import './App.css';
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+
+import uuid from 'react-uuid';
+
+const listOfInputTasks = [
+
+  {id: uuid(), content: 'Task 1'}, 
+  {id: uuid(), content: 'Task 2'}, 
+  {id: uuid(), content: 'Task 3'}, 
+  {id: uuid(), content: 'Task 4'}
+];
+
+
+const MajorTaskColumns = 
+  {
+    [uuid()]: {
+      name: 'TODOs', 
+      tasks: listOfInputTasks
+
+    }
+  };
 
 function App() {
+  const [columns, setColumns]  = useState (MajorTaskColumns);
+
   return (
-    <div className="App">
-      <h1>Aben's Kanban Board</h1>
+    <div style = {{ display: 'flex', justifyContent:'center', height: '100%'}}>
+      
+      <DragDropContext onDropEnd = {result => console.log(result)}>
+      
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <Droppable droppableId = {id}>
+              {(provided, snapshot) => {
+                return(
+
+                  <div 
+                  {...provided.droppableProps} 
+                  ref  = {provided.innerRef} 
+                  style = {{
+                    backgroundColor: snapshot.isDraggingOver ? 'grey': 'lightgreen',
+                    padding: 6,
+                    width: 300,  
+                    minHeight: 400
+                  }}
+                  >
+                    {column.tasks.map((task, index) => {
+
+                      return(
+
+                        <Draggable key  = {task.id} draggableId = {task.id} index = {index}>
+                          {(provided, snapshot) => {
+
+                            return (
+                              <div 
+                              ref = {provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              style = {{
+                                
+                                userSelect: 'none',
+                                padding: 18,
+                                margin: '0 0 10px 0',
+                                minHeight: '60px',
+                                backgroundColor:  snapshot.isDragging ? 'yellow' : 'red',
+                                ...provided.draggableProps.style
+                              }}
+
+
+                              
+                              >
+                                
+
+                                {task.content}
+
+
+                              </div>
+
+
+                            )
+                          }}
+
+
+                        </Draggable>
+                      );
+                    })}
+                    
+                    
+                  </div>
+                )
+              }}
+              
+              </Droppable>
+
+
+
+          )
+
+            })}
+
+
+      </DragDropContext>
 
     </div>
   );
